f2be128f95c3b8d7ad1b1e4f9c51d65ebb61d983
Vinay Ganesh <vganesh@soliantconsulting.com>
2013-11-22 22:17:28 -0800

    Initial commit

---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Album\Model\Album;
+use Album\Form\AlbumForm;
+use Zend\View\Model\JsonModel;
+use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Select;
+
+class AlbumController extends AbstractActionController
+{
+    protected $albumTable;
+
+    public function indexAction()
+    {
+        return new ViewModel(array(
+            'albums' => $this->getAlbumTable()->fetchAll()
+
+        ));
+    }
+
+    public function addAction()
+    {
+        $form = new AlbumForm();
+        $form->get('submit')->setAttribute('value', 'Add');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $album = new Album();
+            $form->setInputFilter($album->getInputFilter());
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                $album->exchangeArray($form->getData());
+                $this->getAlbumTable()->saveAlbum($album);
+
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('album');
+            }
+        }
+
+        return array('form' => $form);
+    }
+
+    public function editAction()
+    {
+        $id = (int)$this->params('id');
+        if (!$id) {
+            return $this->redirect()->toRoute('album', array('action'=>'add'));
+        }
+        $album = $this->getAlbumTable()->getAlbum($id);
+
+        $form = new AlbumForm();
+        $form->bind($album);
+        $form->get('submit')->setAttribute('value', 'Edit');
+        
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                $this->getAlbumTable()->saveAlbum($album);
+
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('album');
+            }
+        }
+
+        return array(
+            'id' => $id,
+            'form' => $form,
+        );
+    }
+
+    public function deleteAction()
+    {
+        $id = (int)$this->params('id');
+        if (!$id) {
+            return $this->redirect()->toRoute('album');
+        }
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $del = $request->getPost()->get('del', 'No');
+            if ($del == 'Yes') {
+                $id = (int)$request->getPost()->get('id');
+                $this->getAlbumTable()->deleteAlbum($id);
+            }
+
+            // Redirect to list of albums
+            return $this->redirect()->toRoute('album');
+        }
+
+        return array(
+            'id' => $id,
+            'album' => $this->getAlbumTable()->getAlbum($id)
+        );
+    }
+
+    public function searchAction()
+    {
+        $form = new AlbumForm;
+        $form->get('submit')->setAttribute('value', 'Search');
+
+        $request = $this->getRequest();
+
+        if ($request->isPost()) {
+            $album = new Album();
+            $form->setInputFilter($album->getInputFilter());
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                $album->exchangeArray($form->getData());
+            }
+        }
+
+        $artist = $request->getPost('artist');
+        $title = $request->getPost('title');
+
+        $search = array(
+                    'artist' => $artist,
+                    'title' => $title
+                );
+
+
+        foreach ($search as $key => $value) {
+            if (empty($value)) {
+                unset($search[$key]);
+            }
+        }
+
+        if (count($search) > 0) {
+            $result = $this->getAlbumTable()->select($search);
+        }
+        else {
+            $result = array();
+        }
+
+        return array('form' =>$form, 'albums'=> $result);
+    }
+
+    public function getAlbumTable()
+    {
+        if (!$this->albumTable) {
+            $sm = $this->getServiceLocator();
+            $this->albumTable = $sm->get('Album\Model\AlbumTable');
+        }
+        return $this->albumTable;
+    }    
+}
---
+++ module/Album/src/Album/Model/AlbumTable.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Album\Model;
+
+use Zend\Db\TableGateway\AbstractTableGateway;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Sql\Select;
+
+class AlbumTable extends AbstractTableGateway
+{
+    protected $table = 'album';
+
+    public function __construct(Adapter $adapter)
+    {
+        $this->adapter = $adapter;
+        $this->resultSetPrototype = new ResultSet();
+        $this->resultSetPrototype->setArrayObjectPrototype(new Album());
+
+        $this->initialize();
+    }
+
+    public function fetchAll()
+    {
+        $resultSet = $this->select(function(Select $select){
+        $select
+            ->limit(10)
+            ->offset(5);
+        });
+    
+        return $resultSet;
+    }
+
+    public function fetchAllWithSelect(Select $select = null)
+    {
+        if (null === $select)
+            $select = new Select();
+        $select->from($this->table);
+        $resultSet = $this->selectWith($select);
+        $resultSet->buffer();
+        return $resultSet;
+
+    }
+
+    public function getAlbum($id)
+    {
+        $id  = (int) $id;
+        $rowset = $this->select(array('id' => $id));
+        $row = $rowset->current();
+        if (!$row) {
+            throw new \Exception("Could not find row $id");
+        }
+        return $row;
+    }
+
+    public function saveAlbum(Album $album)
+    {
+        $data = array(
+            'artist' => $album->artist,
+            'title'  => $album->title,
+        );
+
+        $id = (int)$album->id;
+        if ($id == 0) {
+            $this->insert($data);
+        } else {
+            if ($this->getAlbum($id)) {
+                $this->update($data, array('id' => $id));
+            } else {
+                throw new \Exception('Form id does not exist');
+            }
+        }
+    }
+
+    public function deleteAlbum($id)
+    {
+        $this->delete(array('id' => $id));
+    }
+
+}
